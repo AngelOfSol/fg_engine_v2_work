413029bed559db4a7619148459032cac1dd43920
Angel of Sol <julienckilian@gmail.com>
2019-06-24 04:53:35 -0400

    Basic animation impl.

---
+++ src/animation/mod.rs
@@ -0,0 +1,7 @@
+mod sprite;
+mod animation;
+
+pub use animation::RenderAnimation as Animation;
+pub use animation::DataAnimation;
+pub use sprite::RenderSprite as Sprite;
+pub use sprite::DataSprite;
