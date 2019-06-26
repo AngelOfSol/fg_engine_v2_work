c2a129614c612ed65026951b70448ccd4bdac7ed
Angel of Sol <julienckilian@gmail.com>
2019-06-26 15:24:54 -0400

    WIP.

--- src/animation/sprite.rs
+++ src/animation/sprite.rs
@@ -15,6 +15,32 @@ pub struct Sprite {
 	pub rotation: f32,
 }
 
+pub fn load_image<S: Into<String>>(
+	key: S,
+	path: &str,
+	ctx: &mut Context,
+	assets: &mut Assets,
+) -> GameResult<()> {
+	let key = key.into();
+	let image = match graphics::Image::new(ctx, path) {
+		Ok(result) => result,
+		Err(GameError::ResourceNotFound(_, _)) => {
+			let raw_image = lodepng::decode32_file(path).map_err(|_| {
+				GameError::ResourceNotFound(format!("image not valid at '{}'", path), vec![])
+			})?;
+			graphics::Image::from_rgba8(
+				ctx,
+				raw_image.width as u16,
+				raw_image.height as u16,
+				&raw_image.buffer.as_bytes(),
+			)?
+		}
+		Err(err) => return Err(err),
+	};
+	assets.images.insert(key, image);
+	Ok(())
+}
+
 
 impl Sprite {
 	pub fn new<S: Into<String>>(path: S) -> Self {
@@ -25,39 +51,11 @@ impl Sprite {
 		}
 	}
 
-	pub fn rename_image(&mut self, new_name: String, assets: &mut Assets) {
-		let asset = assets.images.remove(&self.image);
-		match asset {
-			Some(asset) => { assets.images.insert(new_name.clone(), asset); },
-			None => (),
-		}
-		self.image = new_name;
-	}
-
 	pub fn load_image(&self, ctx: &mut Context, assets: &mut Assets) -> GameResult<()> {
 		if assets.images.contains_key(&self.image) {
 			return Ok(());
 		}
-		let image = match graphics::Image::new(ctx, &self.image) {
-			Ok(result) => result,
-			Err(GameError::ResourceNotFound(_, _)) => {
-				let raw_image = lodepng::decode32_file(&self.image).map_err(|_| {
-					GameError::ResourceNotFound(
-						format!("image not valid at '{}'", self.image),
-						vec![],
-					)
-				})?;
-				graphics::Image::from_rgba8(
-					ctx,
-					raw_image.width as u16,
-					raw_image.height as u16,
-					&raw_image.buffer.as_bytes(),
-				)?
-			}
-			Err(err) => return Err(err),
-		};
-		assets.images.insert(self.image.clone(), image);
-		Ok(())
+		load_image(self.image.clone(), &self.image, ctx, assets)
 	}
 	pub fn draw(
 		ctx: &mut Context,
@@ -76,6 +74,11 @@ impl Sprite {
 					sprite.offset.x - f32::from(image.width()) / 2.0,
 					sprite.offset.y - f32::from(image.height()),
 					0.0,
+				)
+				.to_homogeneous()
+				* nalgebra::Rotation3::from_axis_angle(
+					&nalgebra::Vector3::z_axis(),
+					sprite.rotation,
 				)
 				.to_homogeneous(),
 		);
