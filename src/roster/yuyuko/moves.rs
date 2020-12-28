985aa76416770a091b7f634dca6f4d5a58bd77c5
AngelOfSol <julietckilian@gmail.com>
2020-12-28 12:58:41 -0500

    New graphics ids.

--- src/roster/yuyuko/moves.rs
+++ src/roster/yuyuko/moves.rs
@@ -2,6 +2,8 @@ use inspect_design::Inspect;
 use serde::{Deserialize, Serialize};
 use strum::Display;
 
+use super::YuyukoGraphic;
+
 #[derive(Clone, Copy, Debug, Serialize, Deserialize, Hash, PartialEq, Eq, Display, Inspect)]
 #[serde(rename_all = "snake_case")]
 pub enum MoveId {
@@ -86,6 +88,64 @@ impl MoveId {
             .unwrap()
             .trim_matches('\"')
             .to_owned()
+    }
+
+    pub fn into_graphic_id(self) -> YuyukoGraphic {
+        match self {
+            MoveId::Stand => YuyukoGraphic::Stand,
+            MoveId::WalkBackward => YuyukoGraphic::WalkBackward,
+            MoveId::WalkForward => YuyukoGraphic::WalkForward,
+            MoveId::Attack5A => YuyukoGraphic::Attack5A,
+            MoveId::Attack2A => YuyukoGraphic::Attack2A,
+            MoveId::Attack5B => YuyukoGraphic::Attack5B,
+            MoveId::Attack3B => YuyukoGraphic::Attack3B,
+            MoveId::Attack2B => YuyukoGraphic::Attack2B,
+            MoveId::Attack6B => YuyukoGraphic::Attack6B,
+            MoveId::Attack5C => YuyukoGraphic::Attack5C,
+            MoveId::Attack2C => YuyukoGraphic::Attack2C,
+            MoveId::Air5A => YuyukoGraphic::Air5A,
+            MoveId::Air8A => YuyukoGraphic::Air8A,
+            MoveId::Air5B => YuyukoGraphic::Air5B,
+            MoveId::Air2B => YuyukoGraphic::Air2B,
+            MoveId::Air5C => YuyukoGraphic::Air5C,
+            MoveId::Air2C => YuyukoGraphic::Air2C,
+            MoveId::Crouch => YuyukoGraphic::Crouch,
+            MoveId::ToCrouch => YuyukoGraphic::ToCrouch,
+            MoveId::ToStand => YuyukoGraphic::ToStand,
+            MoveId::ForwardDashStart => YuyukoGraphic::ForwardDashStart,
+            MoveId::ForwardDash => YuyukoGraphic::ForwardDash,
+            MoveId::ForwardDashEnd => YuyukoGraphic::ForwardDashEnd,
+            MoveId::BackDash => YuyukoGraphic::BackDash,
+            MoveId::Jump => YuyukoGraphic::Jump,
+            MoveId::SuperJump => YuyukoGraphic::SuperJump,
+            MoveId::AirIdle => YuyukoGraphic::AirIdle,
+            MoveId::Fly => YuyukoGraphic::Fly,
+            MoveId::FlyStart => YuyukoGraphic::FlyStart,
+            MoveId::FlyEnd => YuyukoGraphic::FlyEnd,
+            MoveId::HitstunStandStart => YuyukoGraphic::HitstunStandStart,
+            MoveId::HitstunStandLoop => YuyukoGraphic::HitstunStandLoop,
+            MoveId::HitstunAirStart => YuyukoGraphic::HitstunAirStart,
+            MoveId::HitstunAirMid1 => YuyukoGraphic::HitstunAirMid1,
+            MoveId::HitstunAirMid2 => YuyukoGraphic::HitstunAirMid2,
+            MoveId::HitstunAirLoop => YuyukoGraphic::HitstunAirLoop,
+            MoveId::BlockstunAirStart => YuyukoGraphic::BlockstunAirStart,
+            MoveId::BlockstunAirLoop => YuyukoGraphic::BlockstunAirLoop,
+            MoveId::BlockstunCrouchStart => YuyukoGraphic::BlockstunCrouchStart,
+            MoveId::BlockstunCrouchLoop => YuyukoGraphic::BlockstunCrouchLoop,
+            MoveId::BlockstunStandStart => YuyukoGraphic::BlockstunStandStart,
+            MoveId::BlockstunStandLoop => YuyukoGraphic::BlockstunStandLoop,
+            MoveId::WrongblockCrouchStart => YuyukoGraphic::WrongblockCrouchStart,
+            MoveId::WrongblockCrouchLoop => YuyukoGraphic::WrongblockCrouchLoop,
+            MoveId::WrongblockStandStart => YuyukoGraphic::WrongblockStandStart,
+            MoveId::WrongblockStandLoop => YuyukoGraphic::WrongblockStandLoop,
+            MoveId::HitGround => YuyukoGraphic::HitGround,
+            MoveId::GetUp => YuyukoGraphic::GetUp,
+            MoveId::BorderEscapeJump => YuyukoGraphic::BorderEscapeJump,
+            MoveId::MeleeRestitution => YuyukoGraphic::MeleeRestitution,
+            MoveId::GuardCrush => YuyukoGraphic::GuardCrush,
+            MoveId::RoundStart => YuyukoGraphic::RoundStart,
+            MoveId::Dead => YuyukoGraphic::Dead,
+        }
     }
 }
 
